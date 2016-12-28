3823645fca3f2a679018766a80eb36cf04cc8ed9
stone <mstone@stonecover.com>
2016-12-27 22:06:36 -0700

    fixed error in scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   enum status:[:active, :pending, :inactive]
   
-  scope :status, -> (status) { where status: status }
+  scope :status, -> (status) { where(status: status) }
   scope :location, -> (location) { where("location like ?", "%#{location}%") }
   scope :keyword, -> (keyword) { where("name like ?", "%#{keyword}%") }
   
